059b36d95ceafefdf7e67f9c10c1714b44d7cb5e
DESKTOP-9VUS62Q\Diem <dinhdiemdlx@gmail.com>
2023-07-05 15:03:19 +0700

    setup database

---
+++ Ecommerce.Data/EF/EcommerceDbContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Data.Configurations;
+using Ecommerce.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Ecommerce.Data.EF
+{
+    public class EcommerceDbContext : DbContext
+    {
+        public EcommerceDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configure using Fluent API
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
+
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactConfiguration());
+            modelBuilder.ApplyConfiguration(new PromotionConfiguration());
+
+            //modelBuilder.ApplyConfiguration(new LanguageConfiguration());
+            //modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            //modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            //modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+            //Data seeding
+            //modelBuilder.Seed();
+            //base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<AppConfig> AppConfigs { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public DbSet<Promotion> Promotions { get; set; }
+
+        // table Translations
+        //public DbSet<Language> Languages { get; set; }
+        //public DbSet<ProductTranslation> ProductTranslations { get; set; }
+        //public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        //public DbSet<Transaction> Transactions { get; set; }
+    }
+
+}
---
+++ Ecommerce.Data/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Data.Enums;
+
+namespace Ecommerce.Data.Entities
+{
+    public class Category
+    {
+        public int Id { set; get; }
+        public int SortOrder { set; get; }
+        public bool IsShowOnHome { set; get; }
+        public int? ParentId { set; get; }
+        public Status Status { set; get; }
+
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        //public List<CategoryTranslation> CategoryTranslations { get; set; }
+
+    }
+}
