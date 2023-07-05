d557f71d57f46c52b04fb38728465fabb010df11
DESKTOP-9VUS62Q\Diem <dinhdiemdlx@gmail.com>
2023-07-05 15:35:43 +0700

    Commit complete Data seeding

---
+++ Ecommerce.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Data.Entities;
+using Ecommerce.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppConfig>().HasData(
+               new AppConfig() { Key = "HomeTitle", Value = "This is home page of Ecommerce" },
+               new AppConfig() { Key = "HomeKeyword", Value = "This is keyword of Ecommerce" },
+               new AppConfig() { Key = "HomeDescription", Value = "This is description of Ecommerce" }
+               );
+            modelBuilder.Entity<Category>().HasData(
+                new Category()
+                {
+                    Id = 1,
+                    IsShowOnHome = true,
+                    ParentId = null,
+                    SortOrder = 1,
+                    Status = Status.Active,
+                },
+                 new Category()
+                 {
+                     Id = 2,
+                     IsShowOnHome = true,
+                     ParentId = null,
+                     SortOrder = 2,
+                     Status = Status.Active
+                 });
+
+            modelBuilder.Entity<Product>().HasData(
+           new Product()
+           {
+               Id = 1,
+               DateCreated = DateTime.Now,
+               OriginalPrice = 100000,
+               Price = 200000,
+               Stock = 0,
+               ViewCount = 0,
+           });
+            modelBuilder.Entity<ProductInCategory>().HasData(
+                new ProductInCategory() { ProductId = 1, CategoryId = 1 }
+                );
+        }
+    }
+}
---
+++ Ecommerce.Data/Migrations/20230705083358_InitialCreateSeed.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ecommerce.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AppConfigs",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "HomeDescription", "This is description of Ecommerce" },
+                    { "HomeKeyword", "This is keyword of Ecommerce" },
+                    { "HomeTitle", "This is home page of Ecommerce" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsShowOnHome", "ParentId", "SortOrder", "Status" },
+                values: new object[,]
+                {
+                    { 1, true, null, 1, 1 },
+                    { 2, true, null, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "DateCreated", "OriginalPrice", "Price", "SeoAlias" },
+                values: new object[] { 1, new DateTime(2023, 7, 5, 15, 33, 58, 593, DateTimeKind.Local).AddTicks(1489), 100000m, 200000m, null });
+
+            migrationBuilder.InsertData(
+                table: "ProductInCategories",
+                columns: new[] { "CategoryId", "ProductId" },
+                values: new object[] { 1, 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeDescription");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeKeyword");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeTitle");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
