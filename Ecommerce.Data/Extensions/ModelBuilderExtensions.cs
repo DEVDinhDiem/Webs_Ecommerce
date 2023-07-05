0d8ac34d504f4df6e5c3cecedb25bad5e4e0acb0
DESKTOP-9VUS62Q\Diem <dinhdiemdlx@gmail.com>
2023-07-05 17:29:15 +0700

    ....

--- Ecommerce.Data/Extensions/ModelBuilderExtensions.cs
+++ Ecommerce.Data/Extensions/ModelBuilderExtensions.cs
@@ -77,13 +77,6 @@ namespace Ecommerce.Data.Extensions
                 LastName = "Diem",
                 Dob = new DateTime(2020, 01, 31)
             });
-            modelBuilder.Entity<CategoryTranslation>().HasData(
-                 new CategoryTranslation() { Id = 1, CategoryId = 1, Name = "Áo nam", LanguageId = "vi-VN", SeoAlias = "ao-nam", SeoDescription = "Sản phẩm áo thời trang nam", SeoTitle = "Sản phẩm áo thời trang nam" },
-                 new CategoryTranslation() { Id = 2, CategoryId = 1, Name = "Men Shirt", LanguageId = "en-US", SeoAlias = "men-shirt", SeoDescription = "The shirt products for men", SeoTitle = "The shirt products for men" },
-                 new CategoryTranslation() { Id = 3, CategoryId = 2, Name = "Áo nữ", LanguageId = "vi-VN", SeoAlias = "ao-nu", SeoDescription = "Sản phẩm áo thời trang nữ", SeoTitle = "Sản phẩm áo thời trang women" },
-                 new CategoryTranslation() { Id = 4, CategoryId = 2, Name = "Women Shirt", LanguageId = "en-US", SeoAlias = "women-shirt", SeoDescription = "The shirt products for women", SeoTitle = "The shirt products for women" }
-                   );
-
 
             modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(new IdentityUserRole<Guid>
             {
