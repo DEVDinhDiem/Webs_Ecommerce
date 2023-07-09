b06018f5e506186f4db7f30279af8512fb0a6306
DESKTOP-9VUS62Q\Diem <dinhdiemdlx@gmail.com>
2023-07-09 10:00:28 +0700

    login jwt

--- Ecommerce.BackendApi/Program.cs
+++ Ecommerce.BackendApi/Program.cs
@@ -4,24 +4,66 @@ using Ecommerce.Utilities.Constants;
 using Ecommerce.Application.Catalog.Products;
 using Microsoft.OpenApi.Models;
 using Ecommerce.Application.Common;
+using Ecommerce.Application.System.Users;
+using Ecommerce.Data.Entities;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddDbContext<EcommerceDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString(SystemConstants.MainConnectionString));
 });
+builder.Services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<EcommerceDbContext>()
+                .AddDefaultTokenProviders();
 
 //Declare DI
 builder.Services.AddTransient<IPublicProductService, PublicProductService>();
 builder.Services.AddTransient<IManageProductService, ManageProductService>();
 builder.Services.AddTransient<IStorageService, FileStorageService>();
+
+builder.Services.AddTransient<UserManager<AppUser>>();
+builder.Services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+builder.Services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+builder.Services.AddTransient<IUserService, UserService>();
+
+builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Ecommerce Solution", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+            Reference = new OpenApiReference
+                {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
 });
 var app = builder.Build();
 
---
+++ Ecommerce.ViewModels/System/Users/LoginRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.ViewModels.System.Users
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+
+        public string Passwrod { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
