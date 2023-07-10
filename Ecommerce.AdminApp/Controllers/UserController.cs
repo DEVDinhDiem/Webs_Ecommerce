e1ca72f1d54ba25c27771fa7e9e26b575d6cd898
DESKTOP-9VUS62Q\Diem <dinhdiemdlx@gmail.com>
2023-07-10 15:56:22 +0700

    commit

---
+++ Ecommerce.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Ecommerce.ViewModels.System.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        //private readonly IUserApiClient _userApiClient;
+
+        //public UserController(IUserApiClient userApiClient)
+        //{
+        //    _userApiClient = userApiClient;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return View(ModelState);
+
+            //var token = await _userApiClient.Authenticate(request);
+            var token = Ok();
+
+            return View(token);
+        }
+    }
+}
